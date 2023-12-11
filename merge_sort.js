7a2c7f633c72992d618c9f10265914b2dca3041e
samiullah <sami606713@gmail.com>
2023-12-11 20:41:24 +0500

    sorting
    merge sort

--- merge_sort.js
+++ merge_sort.js
@@ -1,31 +1,31 @@
-// 2-Helper fun 
-function helper(arr1,arr2) {
+// Helper
+function helper(a1,a2) {
     let i=0
-    let j=0 
+    let j=0
     let merge=[]
-
-    while (i<arr1.length & j<arr2.length) {
-        if (arr1[i]<arr2[j]) {
-            merge.push(arr1[i])
+    
+    while (i<a1.length & j<a2.length) {
+        if (a1[i]<a2[j]) {
+            merge.push(a1[i])
             i+=1
-        }else{
-            merge.push(arr2[j])
+        } else {
+            merge.push(a2[j])
             j+=1
         }
     }
-    while (i<arr1.length) {
-        merge.push(arr1[i])
+    while (i<a1[i]) {
+        merge.push(a1[i])
         i+=1
     }
-    while (j<arr2.length) {
-        merge.push(arr2[j])
+    while (j<a2[j]) {
+        merge.push(a2[j])
         j+=1
     }
     return merge
 }
-// let a1=[4,2,6,10,8]
-// let a2=[1,5,3,9,7]
-// console.log(helper(a1,a2));
+let a1=[4,2,6,10,8]
+let a2=[1,5,3,9,7]
+console.log(helper(a1,a2));
 // 1-Merge sort
 function Merge_Sort(arr) {
     // base conditon
@@ -43,5 +43,7 @@ function Merge_Sort(arr) {
     // return {"left:array:":left_array,"right arry:":right_array}
     return helper(left_array,right_array)
 }
+
 let arr=[3,2,5,4,7,6,9,8,10]
-console.log(Merge_Sort(arr));
+console.log("Unsorted:",arr);
+console.log("Sorted",Merge_Sort(arr));
